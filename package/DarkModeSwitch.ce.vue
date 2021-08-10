41016de614d9adc88753dfd97bca92a738234419
Maximo Mussini <maximomussini@gmail.com>
2021-08-10 19:35:44 -0300

    chore: simplify styles

--- package/DarkModeSwitch.ce.vue
+++ package/DarkModeSwitch.ce.vue
@@ -17,7 +17,7 @@ watch(isDark, reflectDark)
 </script>
 
 <template>
-  <button class="icon-button" aria-label="Toggle theme" @click="toggleDark()">
+  <button aria-label="Toggle theme" @click="toggleDark()">
     <transition name="slide" mode="out-in">
       <ic:twotone-nightlight v-if="isDark"/>
       <ic:twotone-wb-sunny v-else/>
@@ -25,7 +25,7 @@ watch(isDark, reflectDark)
   </button>
 </template>
 
-<style lang="postcss">
+<style>
 :host {
   --color: #fbbf24;
   --bg-normal: #fAfAf9;
@@ -39,7 +39,7 @@ watch(isDark, reflectDark)
   --bg-active: #2d2d2d;
 }
 
-.icon-button {
+button {
   background-color: var(--bg-normal);
   border: none;
   border-radius: .5rem;
@@ -52,8 +52,8 @@ watch(isDark, reflectDark)
   transition: background-color 0.3s ease, color 0.3s cubic-bezier(0.64, 0, 0.78, 0);
 }
 
-.icon-button:hover,
-.icon-button:focus {
+button:hover,
+button:focus {
   background-color: var(--bg-active);
   outline: none;
 }
